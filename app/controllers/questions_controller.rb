cf7f88e5e98141037521997722522ce7fcb62f26
VidaspVii <vidasp@mail.ru>
2019-02-09 17:10:13 +0300

    Creata actions show new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,34 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show edit]
+
   def index
     @questions = Question.all
+  end
+
+  def show; end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit; end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
