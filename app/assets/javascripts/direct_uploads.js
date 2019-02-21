788489eda3bd67c9358a976eb99e9897cac35a7d
Pavel <vidasp@mail.ru>
2019-02-21 14:52:39 +0300

    FIX render edit form

--- app/assets/javascripts/direct_uploads.js
+++ app/assets/javascripts/direct_uploads.js
@@ -31,8 +31,7 @@ addEventListener("direct-upload:error", event => {
 })
 
 addEventListener("direct-upload:end", event => {
-  const { id } = event.detail
+  const { id } = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
-  element.classList.add("direct-upload--complete")
-  element.parentElement.classList.add('hidden');
+  element.classList.add("direct-upload--complete");
 })
