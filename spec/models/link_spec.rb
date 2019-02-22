9b4e4b5c356d06b7ea7f41133349c7877924fdcf
Pavel <vidasp@mail.ru>
2019-02-22 12:13:32 +0300

    Add validates link.url at format

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Link, type: :model do
   it { should belong_to :linkable }
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+  it { should allow_value('http://asd.ru').for :url }
+  it { should_not allow_value('asd.ru').for :url }
 end
