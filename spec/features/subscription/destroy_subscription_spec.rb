2f6fddc3f15a21ebb8dbdb2f740c4f0648698444
VidaspVii <vidasp@mail.ru>
2019-03-15 11:17:45 +0300

    Add spec for unsubscription

--- spec/features/subscription/destroy_subscription_spec.rb
+++ spec/features/subscription/destroy_subscription_spec.rb
@@ -4,9 +4,9 @@ feature 'Authenticated user can cancel subscription' do
   let(:question) { create(:question) }
   let(:user) { create(:user) }
 
-  scenario 'unsubscribe', js: true do
-    user.subscriptions.create!(question: question)
+  background { user.subscriptions.create!(question: question) }
 
+  scenario 'unsubscribe', js: true do
     sign_in(user)
     visit question_path(question)
 
@@ -17,5 +17,11 @@ feature 'Authenticated user can cancel subscription' do
       sleep 1
       expect(page).to have_content 'Subscribe to the update issue'
     end
+  end
+
+  scenario 'unauthenticated user unable unsubscribe' do
+    visit question_path(question)
+
+    expect(page).to_not have_content 'Unsubscribe'
   end
 end
